7dba6566f25a2844f98019dc8b573ded88f5b933
VechkanovVV <hatavidit84@gmail.com>
2023-07-19 21:26:16 +0300

    add plugin

--- net_permission_handler/lib/src/utils.dart
+++ net_permission_handler/lib/src/utils.dart
@@ -1,21 +1,14 @@
 import '../connectivity.dart';
 
 
-/// Convert a String to a ConnectivityResult value.
+
 ConnectivityResult parseConnectivityResult(String state) {
   switch (state) {
-    case 'bluetooth':
-      return ConnectivityResult.bluetooth;
+
     case 'wifi':
       return ConnectivityResult.wifi;
-    case 'ethernet':
-      return ConnectivityResult.ethernet;
     case 'mobile':
       return ConnectivityResult.mobile;
-    case 'vpn':
-      return ConnectivityResult.vpn;
-    case 'other':
-      return ConnectivityResult.other;
     case 'none':
     default:
       return ConnectivityResult.none;
