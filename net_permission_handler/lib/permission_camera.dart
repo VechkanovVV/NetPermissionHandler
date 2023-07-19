4b7141f70d482496588b797aa23c291d5103d157
VechkanovVV <hatavidit84@gmail.com>
2023-07-20 01:20:34 +0300

    add permission for camera

--- net_permission_handler/lib/permission_camera.dart
+++ net_permission_handler/lib/permission_camera.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 class PermissionCamera{
 
   static Future<bool> requestCameraPermission() async {
-    MethodChannel _channel = const MethodChannel('dev.fluttercommunity.plus/connectivity');
+    MethodChannel _channel = const MethodChannel('dev/connectivity');
     final bool granted = await _channel.invokeMethod('requestCameraPermission');
     return granted;
   }
