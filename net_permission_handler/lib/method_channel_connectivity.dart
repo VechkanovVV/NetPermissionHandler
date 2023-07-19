cb3aa2349b19add5a29935f276d5fa83a75c6347
VechkanovVV <hatavidit84@gmail.com>
2023-07-19 21:49:11 +0300

    change plugin

--- net_permission_handler/lib/method_channel_connectivity.dart
+++ net_permission_handler/lib/method_channel_connectivity.dart
@@ -11,26 +11,15 @@ import 'src/utils.dart';
 
 class MethodChannelConnectivity extends ConnectivityPlatform {
 
-  @visibleForTesting
-  MethodChannel methodChannel =
-      const MethodChannel('dev.fluttercommunity.plus/connectivity');
+
+  MethodChannel methodChannel = const MethodChannel('dev.fluttercommunity.plus/connectivity');
 
 
-  @visibleForTesting
-  EventChannel eventChannel =
-      const EventChannel('dev.fluttercommunity.plus/connectivity_status');
+
+  EventChannel eventChannel = const EventChannel('dev.fluttercommunity.plus/connectivity_status');
 
   Stream<ConnectivityResult>? _onConnectivityChanged;
 
-
-  @override
-  Stream<ConnectivityResult> get onConnectivityChanged {
-    _onConnectivityChanged ??= eventChannel
-        .receiveBroadcastStream()
-        .map((dynamic result) => result.toString())
-        .map(parseConnectivityResult);
-    return _onConnectivityChanged!;
-  }
 
   @override
   Future<ConnectivityResult> checkConnectivity() {
