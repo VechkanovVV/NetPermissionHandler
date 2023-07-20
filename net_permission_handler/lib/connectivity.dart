60f60a3648b87db20f2d1874f4e85f38f96e9c1b
VechkanovVV <hatavidit84@gmail.com>
2023-07-20 04:36:41 +0300

    adding state manegment

--- net_permission_handler/lib/connectivity.dart
+++ net_permission_handler/lib/connectivity.dart
@@ -1,32 +1,17 @@
-
-
 import 'dart:async';
-
 import 'connectivity_platform_interface.dart';
 
-// Export enums from the platform_interface so plugin users can use them directly.
-export 'connectivity_platform_interface.dart'
-    show ConnectivityResult;
-
-
-
+export 'connectivity_platform_interface.dart' show ConnectivityResult;
 
 class Connectivity {
-  static Connectivity? _instance;
-  static ConnectivityPlatform _platform = ConnectivityPlatform.instance;
+  static final Connectivity _instance = Connectivity._();
+  static final ConnectivityPlatform _platform = ConnectivityPlatform.instance;
 
-  factory Connectivity() {
-    _instance ??= Connectivity._();
-    return _instance!;
-  }
+  factory Connectivity() => _instance;
 
   Connectivity._();
 
-  Stream<ConnectivityResult> get onConnectivityChanged {
-    return _platform.onConnectivityChanged;
-  }
+  Stream<ConnectivityResult> get onConnectivityChanged => _platform.onConnectivityChanged;
 
-  Future<ConnectivityResult> checkConnectivity() {
-    return _platform.checkConnectivity();
-  }
+  Future<ConnectivityResult> checkConnectivity() => _platform.checkConnectivity();
 }
